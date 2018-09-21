10328c15f82ebc4d58d5ca80d9a775c22674bdf0
Yinkci Heart Entertainment Studio <yinkciworks@gmail.com>
2018-09-21 19:35:22 +0800

    adding a function 'add page' on dashboard

--- index.php
+++ index.php
@@ -1,2 +1,21 @@
-<?php $my_site = $_GET['page']; 
-echo "PAGE: ".$my_site; ?>
+<?php 
+require ("database.php"); 
+define('__ROOTs__', dirname(dirname(__FILE__))); 
+require ("header.php"); 
+
+
+
+
+$PageName = $_GET['page']; 
+$query = mysqli_query($con,"SELECT * FROM cs_page where PageName = '$PageName'  ");
+		$row = mysqli_num_rows($query);
+// Check page if exist
+if ($row==1) {
+	$namae = $row['PageName'];
+	echo $namae;
+}
+else{
+	echo "invalid password";
+
+}
+	mysqli_close($con);
--- page.php
+++ page.php
@@ -1,30 +1,13 @@
-<?php require ("dashboard-header.php"); ?>
-	<div class="col-md-12 col-lg-12 col-xl-10 dash-container">
-<!-- your code here -->
+<?php 
+require ("database.php"); 
+define('__ROOTs__', dirname(dirname(__FILE__))); 
+require ("header.php"); 
 
-<div class="col-md-12">
-	<a href="add-page" class="secprimary_btn">ADD NEW PAGE</a>
-</div>
+	$ses_sqli = mysqli_query($con,"SELECT * FROM cs_page ORDER BY PageID");
+	$x = 1;
 
-<!-- end of your code -->
-	</div>
-</div><!-- END DASHBOARD-HEADER ROW  -->
-<?php
-// $sSQL = sprintf("SELECT page FROM pages WHERE slug = '%s'",
-//     mysql_real_escape_string($_GET['slug'])
-// );
 
-// $rResult = mysql_query($sSQL);
-
-// if(mysql_num_rows($rResult) !== 1)
-// {
-//     header('Location: http://www.yourserver.com/error-page/');
-//     exit;
-// }
-// else
-// {
-//     $aPage = mysql_fetch_assoc($rResult);
-//     echo $aPage['page'];
-//     exit;
-// }
-?>
+	while($row = mysqli_fetch_array($ses_sqli)){
+		$PageName = $row['PageName'];
+		echo $PageName;
+	}
