b9339ee6017d5ca87641d994c76cbbb23db51770
Yinkci <yinkciworks@gmail.com>
2018-09-26 22:32:57 +0800

    Use AngularJS on adding a page in 'Add Page' dashboard

--- add-page.php
+++ add-page.php
@@ -1,6 +1,10 @@
 <?php require ("dashboard-header.php"); ?>
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
 <div class="col-md-12 col-lg-10 col-xl-10 dash-container">
 	<!-- your code here -->
+	<div ng-app="plunker" >
+
+ 
 	<div class="row dashboard-container ">
 		<div class="col-md-9 ">
 			<div class="leftMenu_">
@@ -8,9 +12,9 @@
 				<form method="POST" id="publishpage_id" action="data/addPage">
 				<span class="title-link">
 					<label>ADD TITLE:</label>
-					<input type="text" name="page_title_" value="" class="page_title_"><br>
+					<input type="text"  ng-model="page_title_" name="page_title_" value="" class="page_title_"><br>
 					<label>Link:</label>
-					<?php echo home_url()."/"?><input type="text" name="page_link_" value="" class="page_link_">
+					<?php echo home_url()."/"?><input type="text" name="page_link_" value="{{page_title_|replace: ' ':'-'}}" class="page_link_">
 					<input type="submit" name="publish_page" class="publishpage_" >
 				</span>
 				<select name="template_file" class="template_file">
@@ -45,12 +49,32 @@
 			</div>
 		</div>
 	</div>
-	
+	</div><!-- angular -->
 
 
 	<!-- end of your code -->
 </div>
 </div><!-- END DASHBOARD-HEADER ROW  -->
-    <button class="sai btn">GetHtml</button>
 
+
+<script>
+var app = angular.module('plunker', []);
+
+app.filter('replace', [function () {
+
+    return function (input, from, to) {
+      
+      if(input === undefined) {
+        return;
+      }
+  
+      var regex = new RegExp(from, 'g');
+      return input.replace(regex, to);
+       
+    };
+
+
+}]);
+
+</script>
 
