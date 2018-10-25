5e29bb63627858454fb3e4d46b0cc4a84be9f9ee
Yinkci <yinkciworks@gmail.com>
2018-10-25 15:19:43 +0800

    add database installation

--- install-db.php
+++ install-db.php
@@ -3,6 +3,13 @@ $servername = $_POST['local_name'];
 $username =$_POST['user_name'];
 $password =$_POST['pass_word'];
 $DbName = $_POST['db_name'];
+$prefix = $_POST['prefix_name'];
+$cs_addmenu = $prefix."_cs_addmenu";
+$cs_home_url = $prefix."_cs_home_url";
+$cs_page = $prefix."_cs_page";
+$login = $prefix."_login";
+
+
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
@@ -14,10 +21,54 @@ if ($conn->connect_error) {
 // Create database
 $sql = "CREATE DATABASE ".$DbName;
 if ($conn->query($sql) === TRUE) {
-    echo "Database created successfully";
+
+
+	$conn_table = new mysqli($servername, $username, $password, $DbName);
+
+$table1 = "CREATE TABLE $cs_addmenu (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    USERNAME VARCHAR(30) NOT NULL,
+    EMAIL VARCHAR(40) NOT NULL,
+    DISCOUNT VARCHAR(5),
+    PASSW CHAR(128),
+    ROLE VARCHAR(9)
+    )";
+
+$table2 = "CREATE TABLE $cs_home_url  (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    TITLE VARCHAR(30) NOT NULL,
+    TEXT VARCHAR(30) NOT NULL
+    )";
+
+
+$tables = [$table1, $table2];
+
+foreach($tables as $k => $sql){
+    $query = @$conn_table->query($sql);
+
+    if(!$query)
+       $errors[] = "Table $k : Creation failed ($conn->error)";
+    else
+       $errors[] = "Table $k : Creation done";
+}
+
+
+
 } else {
     echo "Error creating database: " . $conn->error;
 }
+
+
+// sql to create table
+
+
+
+
+
+
+
+
+
 
 $conn->close();
 header("location: install-site");
--- install-site.php
+++ install-site.php
@@ -1,1 +1,22 @@
-install site
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+	<?php include ("inc/links.php");?>
+<body>
+	<form method="POST" action="install-site">
+		<div class="install-site">
+			<img src="assets/img/codeark.png">
+			<label>Your Website Title :</label>
+			<input type="text" name="db_name" placeholder="Website Name">
+			<label>Your Log In username :</label>
+			<input type="text" name="user_name" placeholder="Username">
+			<label>Your Log In password :</label>
+			<input type="text" name="pass_word" placeholder="Password">
+			<input type="submit" name="next" value="Next">
+		</div>
+	</form>
+</body>
+</html>
+
